07b0930e7d87a90d0bba17c8cd7bcc8c18f35ab3
AsBergs <anasofiabergsoares06@gmail.com>
2023-05-02 11:09:56 -0300

    LONGWERETHENIGHTSWHENMYDAYSONCEREVOLVEDAROUNDYOU

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const nome = ref('');
+const email = ref('');
+const senha = ref('');
+const senhaConfirma = ref('');
+const dataNasc = ref('');
+const endereço = ref('');
+const cidade = ref('');
+const estado = ref('');
+const hobbies = ref('');
+const lingprog = ref('');
+const biografia = ref('');
+
+</script>
+
+
+
